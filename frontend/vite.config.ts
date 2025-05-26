f879555957a3e013829ee113fb7095e46ad8dde6
PlutoA713N <mallachandu99@gmail.com>
2025-05-27 04:46:19 +0530

    add console

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default ({ mode }: { mode: string }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  console.log('env:', env, mode, process.cwd());
+  console.log('im logging the code', 'env:', env, mode, process.cwd(), 'upto here');
   console.log('VITE_BACKEND_URL:', env.VITE_BACKEND_URL, env); // Debugging line
   return defineConfig({
     plugins: [react()],
